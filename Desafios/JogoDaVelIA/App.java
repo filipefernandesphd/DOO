c279606d210fad53c6b659d060f016fb6cb54e68
Filipe Fernandes, PhD <filipe.fernandes@ifsudestemg.edu.br>
2026-01-10 18:56:46 -0300

    Jogo da VelIA atualizado

--- Desafios/JogoDaVelIA/App.java
+++ Desafios/JogoDaVelIA/App.java
@@ -1,11 +1,9 @@
 import core.Game;
 
-import ia.Filipe.*;
-
 public class App {
     public static void main(String[] args) {
-        Player jogador1 = new ia.Filipe.Player("FILIPE");
-        Player jogador2 = new ia.Filipe.Player("JOSÉ");
+        Player jogador1 = /* IA */;
+        Player jogador2 = /* IA */;
 
         Game game = new Game(jogador1, jogador2);
         game.start();
